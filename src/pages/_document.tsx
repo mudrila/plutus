c190e6767398df10764495a90c151dc28de0ee8a
Kirill Klimenko <klimenkomud@gmail.com>
2023-02-20 13:48:29 +0200

    Setup MUI

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,73 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import * as React from 'react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import theme, { roboto } from '../theme';
+import createEmotionCache from '../theme/createEmotionCache';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html
+        lang="en"
+        className={roboto.className}
+      >
+        <Head>
+          <meta
+            name="theme-color"
+            content={theme.palette.primary.main}
+          />
+          <link
+            rel="shortcut icon"
+            href="/favicon.ico"
+          />
+          <meta
+            name="emotion-insertion-point"
+            content=""
+          />
+          {(this.props as any).emotionStyleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+Document.getInitialProps = async ctx => {
+  const originalRenderPage = ctx.renderPage;
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) =>
+        function EnhanceApp(props) {
+          return (
+            <App
+              emotionCache={cache}
+              {...props}
+            />
+          );
+        },
+    });
+
+  const initialProps = await NextDocument.getInitialProps(ctx);
+  // This is important. It prevents Emotion to render invalid HTML.
+  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map(style => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  };
+};
---
+++ src/theme/index.ts
@@ -0,0 +1,26 @@
+import { Roboto } from '@next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
